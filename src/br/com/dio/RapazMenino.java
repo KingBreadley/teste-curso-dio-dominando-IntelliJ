db6d1f539f23356172856f876311bec125ced5b5
KingBreadley <rafaelsesicacoal@gmail.com>
2022-07-20 22:07:55 -0400

    primeiro commit

---
+++ src/br/com/dio/RapazMenino.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+
+import br.com.dio.model.Gato;
+
+public class RapazMenino {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        /*int a =5;
+        int b = 444;
+        System.out.println("Oláaaaaaaaaaa " + (a+b));*/
+    }
+}
