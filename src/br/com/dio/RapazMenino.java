835c8a74408ed73fc392071a70936376a949dedc
KingBreadley <rafaelsesicacoal@gmail.com>
2022-07-20 22:17:05 -0400

    criando classe livro

--- src/br/com/dio/RapazMenino.java
+++ src/br/com/dio/RapazMenino.java
@@ -8,8 +8,46 @@ public class RapazMenino {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro = new Livro("teste", 29999);
+        System.out.println(livro);
+
         /*int a =5;
         int b = 444;
         System.out.println("Oláaaaaaaaaaa " + (a+b));*/
+    }
+}
+
+
+class Livro {
+    private String nome;
+    private Integer numPag;
+
+    public Livro(String nome, Integer numPag) {
+        this.nome = nome;
+        this.numPag = numPag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPag() {
+        return numPag;
+    }
+
+    public void setNumPag(Integer numPag) {
+        this.numPag = numPag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPag=" + numPag +
+                '}';
     }
 }
