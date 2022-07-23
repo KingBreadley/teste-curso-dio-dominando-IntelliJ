5ac5a8bca3a1a8eb9e2fd5b020e62ae4bdd2b208
KingBreadley <rafaelsesicacoal@gmail.com>
2022-07-22 21:34:49 -0400

    criando classe livro

---
+++ src/br/com/dio/VariaveisEmJava/VariaveisEmJava.java
@@ -0,0 +1,19 @@
+package br.com.dio.VariaveisEmJava;
+
+public class VariaveisEmJava {
+    public static void main(String[] args) {
+        int i;
+        int I;
+        //int 1a_;
+        //int _1a;
+        //int $aq;
+
+
+        //variável constante
+        final int j =10;
+        //j = 15;
+
+
+
+    }
+}
